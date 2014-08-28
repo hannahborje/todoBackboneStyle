166afde0f65255fe72b3ac805b8852a887164faa
hannahborje <hannah.borjesson@gmail.com>
2014-08-28 16:24:47 +0200

    Removed log. Clean up in comments and indent.

--- static/js/collections/todos.js
+++ static/js/collections/todos.js
@@ -12,9 +12,10 @@ define([
 
 	var TodosCollection = Backbone.Collection.extend({
 
+    // Reference to this collection's model.
     model: Todo,
 
-    // Save all of the todo items
+    // Save all of the todo items under the `"todos"` namespace.
     localStorage: new Store("todos"),
 
     // Filter down the list of all todo items that are finished.
@@ -25,7 +26,20 @@ define([
     // Filter down the list to only todo items that are still not finished.
     remaining: function() {
       return this.without.apply(this, this.done());
+    },
+
+    // We keep the Todos in sequential order, despite being saved by unordered
+    // GUID in the database. This generates the next order number for new items.
+    nextOrder: function() {
+      if (!this.length) return 1;
+      return this.last().get('order') + 1;
+    },
+
+    // Todos are sorted by their original insertion order.
+    comparator: function(todo) {
+      return todo.get('order');
     }
+
   });
   return new TodosCollection;
 });
