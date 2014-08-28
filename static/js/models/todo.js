62adce04a10e4c7711d7efc16ca61263785fce4a
hannahborje <hannah.borjesson@gmail.com>
2014-08-28 16:23:14 +0200

    Removed log. Updated comments.

--- static/js/models/todo.js
+++ static/js/models/todo.js
@@ -1,30 +1,24 @@
 /**
  * Created by hannah on 8/26/14.
  */
-
-console.log("models/todo.js");
-
 define(['underscore', 'backbone'], function(_, Backbone) {
-  var TodoModel = Backbone.Model.extend({
+    var TodoModel = Backbone.Model.extend({
 
-    // Default attributes for the todo.
-    defaults: {
-      content: "empty",
-      done: false
-    },
-
-    // Ensure that each todo created has `content`.
-    initialize: function() {
-      if (!this.get("content")) {
-        this.set({"content": this.defaults.content});
-      }
-    },
-
-    // Toggle the `done` state of this todo item.
-    toggle: function() {
-      this.save({done: !this.get("done")});
-    }
-
-  });
-  return TodoModel;
+        // Default attributes for the todo model
+        defaults: {
+            content: "Empty todo...",
+            done: false
+        },
+        // Check that created todo has content - if not add default attr
+        initialize: function() {
+            if (!this.get("content")) {
+                this.set({"content": this.defaults.content});
+            }
+        },
+        // Saves change if checked or unchecked
+        toggle: function() {
+            this.save({done: !this.get("done")});
+        }
+    });
+    return TodoModel;
 });
