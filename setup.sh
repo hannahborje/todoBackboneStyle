2bd2b61dd1feb7e9aecc1038613f643dc82efd69
hannahborje <hannah.borjesson@gmail.com>
2014-08-29 13:54:42 +0200

    Removed database setup.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+
+# This part uses virtualenv to create a localized python configuration
+
+if [ ! -d venv  ]; then
+    virtualenv venv/
+fi
+
+. venv/bin/activate
+
+pip install -r requirements.txt
